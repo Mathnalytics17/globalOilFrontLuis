c6e06e71acdbdf5ddf16d375450adb07bf9b3549
Luis Molina <luisjose0317@gmail.com>
2025-05-04 14:56:08 -0500

    cambio url documentos2

--- pages/muestras/revision-muestras/revision-muestra/index.js
+++ pages/muestras/revision-muestras/revision-muestra/index.js
@@ -46,7 +46,7 @@ const VistaMuestraIndividual = () => {
   const [docUrl, setDocUrl] = useState('');
 
   const handleOpenDocModal = () => {
-    setDocUrl(`${process.env.NEXT_PUBLIC_API_URL}/documents-list/template-report?muestra=${muestra}`);
+    setDocUrl(`http://82.25.95.14:3000/documents-list/template-report?muestra=${muestra}`);
     setOpenDocModal(true);
   };
   // Cargar todos los resultados
